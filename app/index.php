b0f2f3b906ef0cf879e13edd4edbd71fbc17f3ad
Deffhyper <axelogykk@mail.ru>
2017-04-13 00:53:15 +0300

    add svg icon block

--- app/index.php
+++ app/index.php
@@ -4,8 +4,8 @@
 <div class="top-slider">
     <div style="background-image: url('img/jpeg_files/slider-bg.jpg')">
         <div class="container top-slider__item">
-            <h1 class="top-slider__item--title">Лучшие <span
-                    class="bold-text">средства <br> индивидуальной защиты</span></h1>
+            <p class="top-slider__item--title">Лучшие <span
+                    class="bold-text">средства <br> индивидуальной защиты</span></p>
 
             <p class="top-slider__item--text"><span class="bold-text">Компания «ПромСИЗ»</span> — национальный
                 производитель, одно из крупнейших
@@ -18,8 +18,8 @@
     </div>
     <div style="background-image: url('img/jpeg_files/slider-bg.jpg')">
         <div class="container top-slider__item">
-            <h1 class="top-slider__item--title">Лучшие <span
-                    class="bold-text">средства <br> индивидуальной защиты</span></h1>
+            <p class="top-slider__item--title">Лучшие <span
+                    class="bold-text">средства <br> индивидуальной защиты</span></p>
 
             <p class="top-slider__item--text"><span class="bold-text">Компания «ПромСИЗ»</span> — национальный
                 производитель, одно из крупнейших
@@ -32,8 +32,8 @@
     </div>
     <div style="background-image: url('img/jpeg_files/slider-bg.jpg')">
         <div class="container top-slider__item">
-            <h1 class="top-slider__item--title">Лучшие <span
-                    class="bold-text">средства <br> индивидуальной защиты</span></h1>
+            <p class="top-slider__item--title">Лучшие <span
+                    class="bold-text">средства <br> индивидуальной защиты</span></p>
 
             <p class="top-slider__item--text"><span class="bold-text">Компания «ПромСИЗ»</span> — национальный
                 производитель, одно из крупнейших
@@ -44,6 +44,144 @@
             </div>
         </div>
     </div>
+</div>
+
+<div class="remedies">
+    <div class="container">
+        <ul class="remedies-list">
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-1.svg"></object>
+                </div>
+                <span>Спецодежда</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-2.svg"></object>
+                </div>
+                <span>Спецобувь</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-3.svg"></object>
+                </div>
+                <span>Рабочие перчатки</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-4.svg"></object>
+                </div>
+                <span>Средства защиты <br> глаз и лица</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-5.svg"></object>
+                </div>
+                <span>Средства защиты <br> головы</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-6.svg"></object>
+                </div>
+                <span>Средства защиты <br> органов слуха</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-7.svg"></object>
+                </div>
+                <span>Средства защиты <br> органов дыхания</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-8.svg"></object>
+                </div>
+                <span>ДСИЗ</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-9.svg"></object>
+                </div>
+                <span>Защита от <br> электрического тока</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-10.svg"></object>
+                </div>
+                <span>Защитные системы <br> блокировки LOTO</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-11.svg"></object>
+                </div>
+                <span>Средства защиты <br> от падения с высоты</span>
+            </a>
+        </li>
+        <li class="remedies-item">
+            <a href="#">
+                <div class="remedies-item__image">
+                    <object type="image/svg+xml" data="img/svg-image/cat-12.svg"></object>
+                </div>
+                <span>Другое</span>
+            </a>
+        </li>
+    </ul>
+    </div>
+</div>
+
+<div class="block-with-back">
+    <div class="container">
+        <div class="block-with-back__title text-center">
+            <h1 class="block-title">Распродажа</h1>
+            <p class="block-subtitle text-center">Лучшие ценовые предложения</p>
+        </div>
+        <div class="four-item-slider">
+            <div>
+                <div class="goods">
+                    <div class="goods-image">
+                        <a href="#">
+                            <img src="img/jpeg_files/goods-1.jpg" alt="goods">
+                        </a>
+                    </div>
+                    <div class="goods-prop">
+                        <div class="goods-prop__name">
+                            <a href="#">Перчатки антивибрационные Mechanix AV</a>
+                        </div>
+                        <div class="goods-price">
+                            <s class="goods-price__old">715 грн</s>
+                            <span class="goods-price__current">679 грн</span>
+                        </div>
+                        <div class="goods-show-more">
+                            <a href="#" class="btn">Подробнее</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </div>
 
 <?php include_once "parts/footer.php" ?>
