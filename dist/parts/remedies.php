131db08e739e8c7ad55a40d9e65282184d9d6386
Deffhyper <axelogykk@mail.ru>
2017-05-26 15:31:31 +0300

    change svg to png in "remedies" block. Make it independent BEM

---
+++ dist/parts/remedies.php
@@ -0,0 +1,98 @@
+<ul class="remedies-list">
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                        <img src="img/promSIZ-png/cat-1.png" alt="category">
+                    </span>
+            <span>Спецодежда</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-2.png" alt="category">
+                    </span>
+            <span>Спецобувь</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-3.png" alt="category">
+                    </span>
+            <span>Рабочие перчатки</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-4.png" alt="category">
+                    </span>
+            <span>Средства защиты <br> глаз и лица</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-5.png" alt="category">
+                    </span>
+            <span>Средства защиты <br> головы</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-6.png" alt="category">
+                    </span>
+            <span>Средства защиты <br> органов слуха</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                        <img src="img/promSIZ-png/cat-7.png" alt="category">
+                    </span>
+            <span>Средства защиты <br> органов дыхания</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-8.png" alt="category">
+                    </span>
+            <span>ДСИЗ</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-9.png" alt="category">
+                    </span>
+            <span>Защита от <br> электрического тока</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-10.png" alt="category">
+                    </span>
+            <span>Защитные системы <br> блокировки LOTO</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                         <img src="img/promSIZ-png/cat-11.png" alt="category">
+                    </span>
+            <span>Средства защиты <br> от падения с высоты</span>
+        </a>
+    </li>
+    <li class="remedies-item">
+        <a href="#">
+                    <span class="remedies-item__image">
+                        <img src="img/promSIZ-png/cat-12.png" alt="category">
+                    </span>
+            <span>Другое</span>
+        </a>
+    </li>
+</ul>
